2e6b57e9742d825fffe3c1d1c464cab139e7203c
MeDicenAgus <agustinespin@hotmail.com>
2019-11-10 22:26:48 -0300

    Add project files.

---
+++ WebLibera/Controllers/EntriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebLibera.Models;
+
+namespace WebLibera.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private LiberaModel db = new LiberaModel();
+
+        // GET: Entries
+        public ActionResult Index()
+        {
+            var entries = db.Entries.Include(e => e.User);
+            return View(entries.ToList());
+        }
+
+        // GET: Entries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Username");
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Tittle,Content,UserId")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entries.Add(entry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Username", entry.UserId);
+            return View(entry);
+        }
+
+        // GET: Entries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Username", entry.UserId);
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Tittle,Content,UserId")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Username", entry.UserId);
+            return View(entry);
+        }
+
+        // GET: Entries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Entry entry = db.Entries.Find(id);
+            db.Entries.Remove(entry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebLibera/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLibera.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class User
+    {
+        public User()
+        {
+            this.Entries = new HashSet<Entry>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+
+        public virtual ICollection<Entry> Entries { get; set; }
+    }
+}
